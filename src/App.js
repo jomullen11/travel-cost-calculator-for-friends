0c4b9f1ba070e3fd2733958cf6077a5bcbf9dbc2
Jordan Mullen <jordan.mullen@hotmail.com>
2019-06-11 19:12:03 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useInput } from './hooks-input'
 
-function App() {
+function App () {
+  const { value:person, bind:bindPersonInput, reset:resetPersonInput } = useInput('');
+  const { value:days, bind:bindDaysInput, reset: resetDaysInput } = useInput();
+  const { value:costInput, bind:bindCostInput, reset:resetCostInput } = useInput();
+  const [cost, setCost] = useState(1500)
+  const [people, setPeople] = useState(['Jordan'])
+  const [daysPerPerson, setDaysPerPerson] = useState([5])
+  const [totalDays, setTotalDays] = useState(5)
+  const [pricePerDayPerPerson, setPricePerDayPerPerson] = useState(0)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    resetPersonInput()
+    resetDaysInput()
+  }
+
+  const setPeopleArray = () => {
+    setPeople([...people, person])
+    setDaysPerPerson([...daysPerPerson, parseInt(days)])
+  }
+
+  const totalCostSubmit = (event) => {
+    event.preventDefault()
+    setCost(costInput)
+    resetCostInput()
+  }
+
+  const calculateTotalDaysPerPeople = () => {
+    setTotalDays(daysPerPerson.reduce((a, b) => a + b))
+  }
+
+  const calculatePricePerDayPerPerson = () => {
+    setPricePerDayPerPerson(cost / totalDays)
+  }
+
+
+  useEffect( () => {
+    calculateTotalDaysPerPeople()
+    calculatePricePerDayPerPerson()
+  });
+
+console.log(people)
+console.log(daysPerPerson)
+console.log(totalDays)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="Name" value={person} placeholder='Name' {...bindPersonInput} required/>
+        <input type="number" name="Days Stayed" value={days} placeholder='# of days stayed' {...bindDaysInput} min='1' max='7' required />
+        <input type="submit" value="Submit" onClick={setPeopleArray}/>
+      </form>
+
+      <form onSubmit={totalCostSubmit}> 
+        <input type="number" name='Total Cost' placeholder='Total Cost of Trip' {...bindCostInput} min='500' max='1000000' required />
+        <input type='submit' name='Submit' value='Submit' />
+      </form>
+
+
+      {'The total cost of the trip is: ' + cost} <br/>
+      {'The total amount of days for everyone is: ' + totalDays} <br/>
+      {'The amount owed per day is: ' + pricePerDayPerPerson}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
